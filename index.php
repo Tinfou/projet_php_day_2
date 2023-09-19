c89d9bcda97f4ad91c9088b6368cf514f2bcf6cd
Tinfou <mahery.rak@outlook.fr>
2023-09-19 14:35:28 +0200

    exercice sur les conditions

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "first commit";
+//exercice sur les conditions
+    $age = 18;
+    if($age==18){
+        echo "vous avez l'âge";
+    }
+    else{
+        echo "vous n'avez pas l'âge";
+    }
+ 
 ?>
