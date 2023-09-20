ccc5e028d4589882eda6c3164916f73d34bfb19e
Tinfou <mahery.rak@outlook.fr>
2023-09-20 15:07:48 +0200

    verification d'une valeur dans un tableau avec in_array

--- index.php
+++ index.php
@@ -4,4 +4,12 @@
     for($var = 0; $var<=3; $var++){
         echo $voiture[$var] ."<br/>";
     }
+    if(in_array('hasina', $voiture)){
+
+        echo 'la clé  se trouve dans le tableau';
+    }
+    else{
+
+        echo 'la clé ne se trouve pas dans le tableau';
+    }
 ?>
