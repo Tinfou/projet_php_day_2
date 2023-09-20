835ac2c2172712f8f9748221138bf0e875f1e730
Tinfou <mahery.rak@outlook.fr>
2023-09-20 13:35:26 +0200

    ajout d'un tableau associatif

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
     $voiture = array ('bmw','hyundai','kia');
+
+    $recette = [
+        'titre' => 'ravitoto',
+        'etape' => 'etape 1: andrahona',
+        'auteur' => 'mahery',
+        'bool' => true,
+    ];
+
+?>
 ?>
