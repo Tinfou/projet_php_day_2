7a54894376535c6a735a8505e125ee0501879435
Tinfou <mahery.rak@outlook.fr>
2023-09-20 10:06:56 +0200

    modification : ajout boucle , ajout condition verification si age>18 est vrai ou faux

--- index.php
+++ index.php
@@ -1,14 +1,42 @@
 <?php
 //exercice sur les conditions
-    $age = 1;
+    $age = -1;
     $isBillet = true;
     $isVerification = true;
+    $var = 1;
+    $ageApte = 18;
+    $ageVerifier = null;
 
-    if(($age>=18 && $isBillet == true) || $isVerification == true){
-        echo "vous avez l'âge, vous pouvez entrer";
+    /*if(($age>=18 && $isBillet == true) || $isVerification == true){
+        echo "vous avez l'âge, vous pouvez entrer"."<br/>";
     }
     else{
-        echo "vous n'avez pas l'âge ou votre billet n'est pas valide";
+        echo "vous n'avez pas l'âge ou votre billet n'est pas valide "."<br/>";
+    }*/
+    if($age>0){
+        while($var<$age){
+        $var++;
+        }
+            echo "Valeur marina <br/> ";
     }
- 
+    else{
+        echo "valeur diso <br/> ";
+    }
+    $ageVerifier = $var;
+
+    if($ageVerifier>=$ageApte){
+        $isAdult = true;
+    }
+    else{
+        $isAdult = false;
+    }
+
+    if($isAdult){
+        echo "Vous avez ".$ageVerifier." vous etez autorisé ";
+    }
+    else{
+        echo "NON vous n'etes pas autorisé ".$ageVerifier." "; 
+    }
+
+
 ?>
