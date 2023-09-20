4f31f19b24e35d26d18206360971e16337e0175d
Tinfou <mahery.rak@outlook.fr>
2023-09-20 16:16:26 +0200

    modification : ajout condition si note inferieur à 10

--- index.php
+++ index.php
@@ -2,25 +2,25 @@
     $etudiants = [
         [
             'nom' => 'mahery',
-            'note' => '12',
+            'note' => 12,
             'validation' => true,
         ],
 
         [
             'nom' => 'hasina',
-            'note' => '9',
-            'validation' => false,
+            'note' => 9,
+            'validation' => true,
         ],
 
         [
             'nom' => 'stelly',
-            'note' => '18',
+            'note' => 18,
             'validation' => true,
         ],
     ];
     foreach ($etudiants as $etudiant){
         //echo $etudiant['nom'] .$etudiant['note'].$etudiant['validation'];
-        if($etudiant['validation'] == true){
+        if(($etudiant['validation'] == true)&& $etudiant['note']>=10){
             echo $etudiant['nom']." afaka" ."<br/>";
         }
         else{
