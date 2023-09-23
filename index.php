81b1671a72f9db473711e19c02666abf65e739a0
Tinfou <mahery.rak@outlook.fr>
2023-09-23 15:07:09 +0200

    var dump method :GET ,POST

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <?php
-     //$voitures = [8 ,6 ,13 ,19 , 14, 5, 3];
+ echo "REQUEST : ";
+   var_dump($_REQUEST);
+echo "<br>GET : ";
+var_dump($_GET);
+echo "<br> POST : ";
+var_dump($_POST);
 
-    /*$voitures = [
-        "SUV" => "hyundai",
-        "4*4" => ["land rover",["110","109"]], 
-        "plaisir" =>["bmw","kia"],
-    ];
-    
+echo "<br>";
 
-    echo $voitures["4*4"][1][1];*/
-
-    for ($var = 0; $var< count($voitures); $var++) {
-        //echo $voitures[$var] ."<br>";
-        if(($voitures[$var]%2) !=0){
-            echo "impaire: ".$voitures[$var] ."<br>";
-        }
-        else{
-            echo "paire: ".$voitures[$var] ."<br>";
-        }
-    }
+echo " <br> bonjour ". $_GET["name"] ." ny surnom anao dia " . $_GET["surnom"] ;
 ?>
