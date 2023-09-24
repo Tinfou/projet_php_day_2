bb6325fb3c9cf3427469a1810ce6667f1aa6f922
Tinfou <144536026+Tinfou@users.noreply.github.com>
2023-09-24 21:30:21 +0200

    ajout fonction :resultat en focntion du note saisie

--- index.php
+++ index.php
@@ -2,18 +2,29 @@
 <form action="./index.php" method="post">
     <p><input type="text" placeholder="Nom" name="nom" required></p>
     <p><input type="text" placeholder="Prenom" name="prenom" required></p>
-    <p><input type="text" placeholder="Telephone" name="telephone" required></p>
+    <p><input type="number" placeholder="Note" name="note" required></p>
     <p><button type="submit" > valider </button></p>
 </form>
 
-
 <?php
-
-    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['telephone'])){
-        echo "Vous avez entrez les informations suivantes : <br>";
+//$note = isset($_POST['note']);
+    if(isset($note)){
+        $note = $_POST['note'];
+    }
+function resultat($note){
+    if($note>=10){
+        echo " <br> Admis";
+    }
+    else{
+        echo " <br> Non admis";
+    }
+}
+    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['note'])){
+        echo "Voici votre resultat";
         echo " <br> Nom : ".$_POST['nom']."<br>";
         echo "Prenom : ".$_POST['prenom']."<br>";
-        echo "Telephone : ".$_POST['telephone']."<br>";
+        echo "note : ".$_POST['note']."<br>";
+        echo resultat($note);
     }
     else{
         echo "Vide";
