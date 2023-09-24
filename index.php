b66884e021d7770da8a46d02d444a8d062d14b97
Tinfou <144536026+Tinfou@users.noreply.github.com>
2023-09-24 19:39:50 +0200

    ajout formulaire, utilisation method:post affichage des elements poster en echo

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
+
+<form action="./index.php" method="post">
+    <p><input type="text" placeholder="Nom" name="nom" required></p>
+    <p><input type="text" placeholder="Prenom" name="prenom" required></p>
+    <p><input type="text" placeholder="Telephone" name="telephone" required></p>
+    <p><button type="submit" > valider </button></p>
+</form>
+
+
 <?php
- echo "REQUEST : ";
-   var_dump($_REQUEST);
-echo "<br>GET : ";
-var_dump($_GET);
-echo "<br> POST : ";
-var_dump($_POST);
 
-echo "<br>";
-
-echo " <br> bonjour ". $_GET["name"] ." ny surnom anao dia " . $_GET["surnom"] ;
+    if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['telephone'])){
+        echo "Vous avez entrez les informations suivantes : <br>";
+        echo " <br> Nom : ".$_POST['nom']."<br>";
+        echo "Prenom : ".$_POST['prenom']."<br>";
+        echo "Telephone : ".$_POST['telephone']."<br>";
+    }
+    else{
+        echo "Vide";
+    }
 ?>
